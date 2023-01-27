f6dd1b2ba5bc46a3eb71cc76db30ec23d3f2364e
root <root@DESKTOP-BC5S1BL.localdomain>
2023-01-27 03:03:52 +0300

    duf

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['54.144.140.214', '34.224.63.222']
 
 
 def do_deploy(archive_path):
