9f66202dd236c72dd20dca641a7d921ebf45d0d3
KingKidd4110 <ayiendabrian@gmail.com>
2023-02-07 00:31:21 +0300

    pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,7 @@
 """Start a flask web app
 """
 
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def hello_flask():
     """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
-
